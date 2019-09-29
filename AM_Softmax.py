1fa84cfbcfca1f072f15308f5b3e82a1116ff56c
Priyank Pathak <priyankpathak50@gmail.com>
2019-09-29 19:32:52 -0400

    Update AM_Softmax.py

--- AM_Softmax.py
+++ AM_Softmax.py
@@ -38,7 +38,7 @@ class CrossEntropyLabelSmooth(nn.Module):
 
 
     
-class AM_Softmax(nn.Module):
+class AM_Softmax_v1(nn.Module): #creates the classification layer 
     def __init__(self, m=0.35, s=30, d=2048, num_classes=625, use_gpu=True , epsilon=0.1):
         super(AM_Softmax, self).__init__()
         self.m = m
@@ -56,7 +56,6 @@ class AM_Softmax(nn.Module):
         '''
         x : feature vector : (b x  d) b= batch size d = dimension 
         labels : (b,)
-        classifier : Fully Connected weights of classification layer (dxC), C is the number of classes: represents the vectors for class
         '''
         # x = torch.rand(32,2048)
         # label = torch.tensor([0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,])
@@ -75,3 +74,38 @@ class AM_Softmax(nn.Module):
         weighted_cos_angle = self.s * cos_angle
         log_probs = self.CrossEntropy(weighted_cos_angle , labels)
         return log_probs
+
+    
+    
+
+class AM_Softmax_v2(nn.Module): #requires classification layer for normalization 
+    def __init__(self, m=0.35, s=30, d=2048, num_classes=625, use_gpu=True , epsilon=0.1):
+        super(AM_Softmax, self).__init__()
+        self.m = m
+        self.s = s 
+        self.num_classes = num_classes
+        self.CrossEntropy = CrossEntropyLabelSmooth(self.num_classes , use_gpu=use_gpu)
+
+    def forward(self, features, labels , classifier  ):
+        '''
+        x : feature vector : (b x  d) b= batch size d = dimension 
+        labels : (b,)
+        classifier : Fully Connected weights of classification layer (dxC), C is the number of classes: represents the vectors for class
+        '''
+        # x = torch.rand(32,2048)
+        # label = torch.tensor([0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,])
+        features = nn.functional.normalize(features, p=2, dim=1) # normalize the features
+        with torch.no_grad():
+            classifier.weight.div_(torch.norm(classifier.weight, dim=1, keepdim=True))
+
+        cos_angle = classifier(features)
+        cos_angle = torch.clamp( cos_angle , min = -1 , max = 1 ) 
+        b = features.size(0)
+        for i in range(b):
+            cos_angle[i][labels[i]] = cos_angle[i][labels[i]]  - self.m 
+        weighted_cos_angle = self.s * cos_angle
+        log_probs = self.CrossEntropy(weighted_cos_angle , labels)
+        return log_probs
+
+
+
